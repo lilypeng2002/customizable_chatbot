d3230eb59b2576867a7e949ac8f49fdd69be39c7
vicomello <vic.oldemburgo@gmail.com>
2023-10-24 20:42:35 -0400

    went back to sql dialect

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ st.markdown(
 
 
 conn = st.experimental_connection('chatrecords',
-    type="mysql",
+    type="sql",
     username=st.secrets["sql_user"],
     password=st.secrets["sql_password"],
     host=st.secrets["sql_host"],
