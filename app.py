4e4a5f628c4f2d81bb735db0005714a6422fdd2f
vicomello <vic.oldemburgo@gmail.com>
2023-10-25 14:09:59 -0400

    removed one of the user is js codes

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ if 'widget_value' not in st.session_state:
 # Set your OpenAI API key here, or use an environment variable
 openai.api_key = st.secrets["API_KEY"]
 
-js_code = """
-<div style="display:none;">
-    <script>
-        setTimeout(function() {
-            const userID = document.getElementById("userID").value;
-            if (userID) {
-                window.Streamlit.setSessionState({"user_id": userID});
-            }
-        }, 1000);  // Delaying the execution by 1 second to ensure DOM is ready
-    </script>
-</div>
-"""
+# js_code = """
+# <div style="display:none;">
+#     <script>
+#         setTimeout(function() {
+#             const userID = document.getElementById("userID").value;
+#             if (userID) {
+#                 window.Streamlit.setSessionState({"user_id": userID});
+#             }
+#         }, 1000);  // Delaying the execution by 1 second to ensure DOM is ready
+#     </script>
+# </div>
+# """
 
-st.markdown(js_code, unsafe_allow_html=True)
+# st.markdown(js_code, unsafe_allow_html=True)
 
 
 # If the user_id hasn't been set in session_state yet, try to retrieve it from the hidden input
