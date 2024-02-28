8c5026a960afe804f2cfe7e26a35557326f73dd5
lilypeng2002 <77036011+lilypeng2002@users.noreply.github.com>
2024-02-28 00:28:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ if 'widget_value' not in st.session_state:
     st.session_state.widget_value = ''
 
 
-
 first = "Kit: Hey there! I’m an AI developed by the University of Toronto, and I’m here to help you explore your desire to become more kind and caring towards others. Can you tell me a little bit more about what’s been on your mind lately? "
 
 # If messages does not exist in state, initialize it
@@ -40,6 +39,7 @@ js_code = """
     </script>
 </div>
 """
+st.set_page_config(layout="wide")
 
 st.markdown(js_code, unsafe_allow_html=True)
 
@@ -51,30 +51,112 @@ current_date = datetime.now().strftime("%Y-%m-%d")
 current_hour = datetime.now().strftime("%H:%M:%S")
 
 st.title('Chatbot')
-st.markdown(f"<div class='message bot'>{first}</div>", unsafe_allow_html=True)
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
+
+# st.markdown("""
+# <style>
+# /* Increase specificity to ensure override and add !important to ensure application */
+# .stTextInput > div > div > input {
+#     position: fixed !important;
+#     bottom: 3rem !important;
+#     height: 40px !important
+#     width: 90% !important; /* Adjust the width as needed */
+#     left: 5% !important; 
+#     z-index: 100 !important; /* Ensure it's above other elements */
+# }
+# </style>
+# """, unsafe_allow_html=True)
+
+
+
+# st.markdown("""
+# <div class="chatContainer">
+#     <div id="messages">
+#     </div>
+#     <div class="chatArea">
+#         <form action="" id="cSubmitButton">
+#             <input id="chatInput" autocomplete="off" placeholder="Type your guess here!"/>
+#         </form>
+#     </div>
+# </div>
+# """, unsafe_allow_html=True)
+
+# st.markdown("""
+# <style>
+
+# .message {
+#             margin: 10px;
+#             padding: 10px;
+#             border-radius: 10px;
+#             width: 70%;
+#         }
+#         .user {
+#             margin-left: auto;
+#             background-color: #2D2928;
+#         }
+#         .bot {
+#             margin-right: auto;
+#             background-color: #2D2928;
+#         }
+
+# .chatArea {
+#     position: fixed;
+#     left: 50%;
+#     top: 50%;
+#     transform: translate(-50%, -50%);
+#     padding: 10px;
+    
+#     border-radius: 10px;
+
+#     width: 90%;
+#     bottom: 0;
+#     display: inline-block;
+#     margin-top: 80%;
+#     background-color: #2D2928; 
+#     justify-content: center; /* Center children horizontally */
+#     align-items: center;
+# }
+
+# #chatInput {
+#     width: calc(100% - 24px); /* Adjust width to account for padding */
+#     padding: 6px 12px;
+#     margin: 0 12px; /* Center the input within the form */
+#     box-sizing: border-box; /* Include padding in width calculation */
+# }
+
+# .cSubmitButton {
+#     width: 80%;
+#     display: inline-block;
+# }
+# </style>
+# """, unsafe_allow_html=True)
+
 
 # Custom CSS for the chat interface
-st.markdown(
-    """
-    <style>
-        .message {
-            margin: 10px;
-            padding: 10px;
-            border-radius: 10px;
-            width: 70%;
-        }
-        .user {
-            margin-left: auto;
-            background-color: #2D2928;
-        }
-        .bot {
-            margin-right: auto;
-            background-color: #2D2928;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# st.markdown(
+#     """
+#     <style>
+#         .message {
+#             margin: 10px;
+#             padding: 10px;
+#             border-radius: 10px;
+#             width: 70%;
+#         }
+#         .user {
+#             margin-left: auto;
+#             background-color: #2D2928;
+#         }
+#         .bot {
+#             margin-right: auto;
+#             background-color: #2D2928;
+#         }
+#     </style>
+#     """,
+#     unsafe_allow_html=True,
+# )
 
 
 
@@ -140,32 +222,40 @@ start_message = {
 
 }
 
+# Display chat messages
+for msg in st.session_state.messages:
+    st.markdown(f"<div class='message {msg['class']}'>{msg['text']}</div>", unsafe_allow_html=True)
 
-
-# user_input = st.text_input("You: ", value=st.session_state.widget_value, on_change=submit, key='widget_value')
-# user_input = st.chat_input("Say something", on_submit=submit)
+# Display modified text input
+user_input = st.text_input("You: ", value=st.session_state.widget_value, on_change=submit, key='widget_value')
 
 if 'chat' not in st.session_state:
     st.session_state.chat = []
-    
+    # st.session_state.chat.append(first)
 
-if st.chat_input("Say something"):
+
+if prompt:
     st.session_state.messages.append({'class': 'user', 'text': f"You: {st.session_state.last_submission}"})
     user_message = {"role": "user", "content": st.session_state.last_submission}
     st.session_state.chat.append(user_message)
+
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=0.2,
         max_tokens=2000,
         messages=[start_message, *st.session_state.chat]
     )
+
     bot_response = response['choices'][0]['message']['content']
     bot_message = {"role": "system", "content": bot_response}
     st.session_state.chat.append(bot_message)
     st.session_state.messages.append({'class': 'bot', 'text': f"Kit: {bot_response}"})
+
     # Save the conversation to SQLite
     conversation_content = f"You: {st.session_state.last_submission}\nBot: {bot_response}"
     save_conversation(conversation_content)
+    #st.write(conversation_content)
     
     st.session_state.last_submission = ''
+    #st.write(conversation_content)
     st.rerun()  # Clear input box by rerunning the app
