d29546b187127e9d8d81fc57f6bb99c07ce215f7
ceiroasolans <conrad.eiroa@gmail.com>
2024-03-02 14:06:02 -0800

    update

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ st.markdown("""
             
     .chat-container {
         flex-grow: 1;
-        top: 15rem;
+        top: 105rem;
         margin: 0 auto;
         overflow-y: auto;
         position: relative;
