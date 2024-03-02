f918bf89320e3c2e8f9b46b2fb9f8c8ea7bbe4e3
ceiroasolans <conrad.eiroa@gmail.com>
2024-03-02 15:30:16 -0800

    update

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ st.markdown("""
     }
             
     .chat-header {
-        width: 100rem;
+        /*width: 44rem;*/
         position: fixed;
         top: 0%; /* Increased to move the header lower */
         display: flex;
