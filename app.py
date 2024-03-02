2816326067a061c5f5a1926c50cd438c01d998f2
ceiroasolans <conrad.eiroa@gmail.com>
2024-03-02 15:29:34 -0800

    update

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ st.markdown("""
     }
             
     .chat-header {
-        width: 44rem;
+        width: 100rem;
         position: fixed;
         top: 0%; /* Increased to move the header lower */
         display: flex;
